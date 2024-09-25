f149a391196d4413ca53790ca4a8f7fe417f516c
Aretemii Korshunov <56575740+TvoiaJopa@users.noreply.github.com>
2024-09-25 10:34:48 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,13 +5,7 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-            double luku1 = 10.5;
-            double luku2 = 5.5;
-
-            Console.WriteLine("Luvut yhdessä pötkössä: " + luku1 + luku2);
-            Console.WriteLine("Luvut erikseen listattuna: " + luku1 + " " + luku2);
-            //Console.WriteLine("Luvut erikseen listattuna toisenlaisella koodilla: {0} {1}", luku1, luku2);
-            Console.WriteLine("Lukujen summa on: " + (luku1 + luku2));
+           
 
 
         }
